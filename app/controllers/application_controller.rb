2f604d344da251bb3dd51f7172c38ef5fb3d8f92
Vitor Vidotto <hugo_sirqueira@hotmail.com>
2018-03-10 14:17:24 -0300

    added json api methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,42 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordInvalid,
+              with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def render_with(json)
+    return json_paginate(json) if json.is_a?(ActiveRecord::Relation)
+    render jsonapi: json, include: includes_params,
+           content_type: 'application/vnd.api+json; charset=utf-8'
+  end
+
+  def json_paginate(json)
+    json = json.paginate(page: params[:page], per_page: params[:per_page])
+    render_object(json, links: pagination(json), meta: pagination_meta(json))
+  end
+
+  def render_with_errors(json)
+    render json: json, status: :unprocessable_entity
+  end
+
+  def includes_params
+    params.as_json.fetch('includes', {})
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: exception.record.errors, status: :unprocessable_entity
+  end
+
+  def render_not_found_response(exception)
+    response = {
+      errors: [{
+        title: exception.message,
+        source: exception.backtrace[0],
+        details: ''
+      }]
+    }
+
+    render json: response, status: :not_found
+  end
 end
