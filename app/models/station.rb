23863e7ff7fd1310e388ef700f1b1dbb592f6a31
Vitor Vidotto <hugo_sirqueira@hotmail.com>
2018-03-11 16:44:28 -0300

    added freespots and available bikes in station

--- app/models/station.rb
+++ app/models/station.rb
@@ -11,4 +11,16 @@ class Station < ApplicationRecord
   has_many :trips, class_name: 'Trip',
                    primary_key: 'id',
                    foreign_key: 'final_station_id'
+
+  def free_spots
+    all_bikes =  Bike.all.where(locable_type: 'Station', locable_id: id).size
+    max_capacity - all_bikes
+  end
+
+  def available_bikes
+    available_cd = Bike.conditions[:available]
+    Bike.all.where(condition_cd: available_cd,
+                   locable_type: 'Station',
+                   locable_id: id).size
+  end
 end
--- app/serializable/serializable_station.rb
+++ app/serializable/serializable_station.rb
@@ -7,7 +7,8 @@ class SerializableStation < JSONAPI::Serializable::Resource
 
   attribute(:created_at) { I18n.l(@object.created_at, format: :timestamp) }
   attribute(:updated_at) { I18n.l(@object.updated_at, format: :timestamp) }
-
+  attribute(:available_bikes) { @object.available_bikes }
+  attribute(:free_spots) { @object.free_spots }
   has_many :bikes, as: :locable
   has_many :trips, class_name: 'Trip',
                    primary_key: 'id',
